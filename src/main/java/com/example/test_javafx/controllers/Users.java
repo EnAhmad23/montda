35ad496061d1e0c1da88bf665ea1e5e07d688294
unknown <ahmadhumeid21@gmail>
2023-05-12 15:49:20 +0300

    Ahmad last updates

--- src/main/java/com/example/test_javafx/controllers/Users.java
+++ src/main/java/com/example/test_javafx/controllers/Users.java
@@ -1,12 +1,16 @@
 package com.example.test_javafx.controllers;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Users implements Initializable {
+    @FXML
+    private Button nav_add_section;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
