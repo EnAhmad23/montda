7b901b5c30a8c7d131abf758b6eefefb7841f6d4
unknown <ahmadhumeid21@gmail>
2023-05-15 22:09:38 +0300

    Ahmad last updates

---
+++ src/main/java/com/example/test_javafx/controllers/addLectureTime.java
@@ -0,0 +1,24 @@
+package com.example.test_javafx.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class addLectureTime {
+    @FXML
+    private TextField id_lecId;
+    @FXML
+    private TextField id_insId;
+    @FXML
+    private TextField id_RoomNo;
+    @FXML
+    private TextField id_timeSlot;
+    @FXML
+    private Button btn_add;
+    @FXML
+    private Button id_back_addlecT;
+
+    public void addLecT(){}
+
+    public void back_lecT(){}
+}
