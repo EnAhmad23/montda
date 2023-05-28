fbc90f3a92546aaf311dbd2100fb9e80f4671ec9
unknown <ahmadhumeid21@gmail>
2023-05-28 13:42:42 +0300

    Ahmad last updates

---
+++ src/main/java/com/example/test_javafx/controllers/absent.java
@@ -0,0 +1,54 @@
+package com.example.test_javafx.controllers;
+
+import com.example.test_javafx.models.Course;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class absent {
+
+
+    @FXML
+    private Button search;
+    @FXML
+    private Button export;
+    @FXML
+    private Button update;
+    @FXML
+    private TextField t_id;
+
+    @FXML
+    private TableView<Course> table;
+    @FXML
+    private TableColumn<absent, String> id;
+    @FXML
+    private TableColumn<absent, String> name;
+    @FXML
+    private TableColumn<absent, String> gender;
+    @FXML
+    private TableColumn<absent, String> major;
+    @FXML
+    private TableColumn<absent, String> place;
+    @FXML
+    private TableColumn<absent, String> phone_num;
+    @FXML
+    private TableColumn<absent, String> course_id;
+    @FXML
+    private TableColumn<absent, String> attPercentage;
+
+
+
+    public void exportStudent(){}
+
+    public void UpdateStudent(){}
+
+    public void Back(){}
+
+    public void searchStudent(){}
+
+    public void autoComplete(KeyEvent keyEvent) {
+    }
+}
